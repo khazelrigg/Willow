73bac49078f7acfeee063a9ddc208cb886c96683
Kameron Hazelrigg <khazelrigg@users.noreply.github.com>
2017-10-11 15:06:28 -0500

    Viewer

--- src/kam/hazelrigg/Viewer.java
+++ src/kam/hazelrigg/Viewer.java
@@ -1,18 +1,12 @@
 package kam.hazelrigg;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.File;
 
 public class Viewer extends Application{
 
@@ -22,62 +16,16 @@ public class Viewer extends Application{
 
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("viewer.fxml"));
 
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open file to analyse");
+        Scene scene = new Scene(root, 300, 275);
 
-        ProgressBar bar = new ProgressBar();
-        bar.setVisible(false);
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.getIcons().add(new Image("https://github.com/iconic/open-iconic/blob/master/png/book.png?raw=true"));
 
-        Book chosen = new Book();
-
-        Text titleOfBook = new Text("Nothing has been selected...");
-
-        Button openButton = new Button("Open file");
-
-        Button analyseButton = new Button("Analyse file");
-        analyseButton.setDisable(true);
-
-        openButton.setOnAction((ActionEvent actionEvent) -> {
-            File file = fileChooser.showOpenDialog(primaryStage);
-            if (file != null) {
-                analyseButton.setDisable(false);
-                chosen.setPath(file);
-                chosen.setTitleFromText(file);
-                titleOfBook.setText(chosen.getTitle() + " by " + chosen.getAuthor());
-            }
-        });
-
-        analyseButton.setOnAction(actionEvent -> {
-            if (chosen.getPath() != null) {
-                chosen.analyseText();
-                chosen.writeFrequencies();
-                chosen.makePosGraph();
-                chosen.makeDifficultyGraph();
-                chosen.writeConclusion();
-            }
-        });
-
-        HBox buttons = new HBox(6);
-        buttons.getChildren().addAll(openButton, analyseButton);
-
-        GridPane inputGridPane = new GridPane();
-
-        inputGridPane.setHgap(6);
-        inputGridPane.setVgap(6);
-
-        GridPane.setConstraints(titleOfBook, 0, 0);
-        GridPane.setConstraints(buttons, 0, 1);
-
-        inputGridPane.getChildren().addAll(titleOfBook, buttons);
-        inputGridPane.setPadding(new Insets(12, 12, 12, 12));
-
-
-        primaryStage.setTitle("Word Count");
-        primaryStage.setScene(new Scene(inputGridPane));
-        primaryStage.show();
-
+        stage.show();
     }
 
 }
---
+++ src/kam/hazelrigg/ViewerController.java
@@ -0,0 +1,4 @@
+package kam.hazelrigg;
+
+public class ViewerController {
+}
