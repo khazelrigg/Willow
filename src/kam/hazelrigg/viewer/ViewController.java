4472050861d4ff5d1f64e39335b0498513477b41
Kameron Hazelrigg <kameronn@protonmail.ch>
2017-11-06 17:46:09 -0600

    Potential fix for switchingBooks in viewer. Uses a hashmap of names and Book objects to switch books on list item click. Updated icons for toggle buttons

--- src/kam/hazelrigg/viewer/ViewController.java
+++ src/kam/hazelrigg/viewer/ViewController.java
@@ -32,8 +32,8 @@ public class ViewController {
     // Content
     public ListView<String> resultsFileListView;
 
-    private ImageView posChartImageView;
-    private ImageView difficultyImageView;
+    private ImageView posChartImageView = new ImageView();
+    private ImageView difficultyImageView = new ImageView();
 
     // Bottom status
     public Label statusLabel;
@@ -42,7 +42,7 @@ public class ViewController {
     public VBox diffTab;
 
     private Book book = new Book();
-    private File directory = null;
+    private File seedDirectory = null;
     private Stage chooserPane = new Stage();
 
     private HashMap<String, Book> bookMap = new HashMap<>();
@@ -64,16 +64,10 @@ public class ViewController {
         File file = fileChooser.showOpenDialog(chooserPane);
 
         if (file != null) {
-            directory = null;
+            seedDirectory = null;
             book.setPath(file);
             book.setTitleFromText(file);
-            addFileToList(file, file.getName());
-            // Add image viewers
-            posTab.getChildren().remove(0);
-            posTab.getChildren().add(posChartImageView);
-            diffTab.getChildren().remove(0);
-            diffTab.getChildren().add(difficultyImageView);
-
+            addFileToList(file);
             statusLabel.setText("Opened " + book.getTitle());
             runButton.setDisable(false);
         }
@@ -92,7 +86,7 @@ public class ViewController {
 
         File dir = directoryChooser.showDialog(chooserPane);
         if (dir != null) {
-            directory = dir;
+            seedDirectory = dir;
             statusLabel.setText("Opened " + dir.getName());
             runButton.setDisable(false);
         }
@@ -103,13 +97,17 @@ public class ViewController {
      */
     public void run() {
         OutputWriter ow = new OutputWriter(book);
-
-        if (directory != null) {
+        // Add image viewers
+        posTab.getChildren().remove(0);
+        posTab.getChildren().add(posChartImageView);
+        diffTab.getChildren().remove(0);
+        diffTab.getChildren().add(difficultyImageView);
+        if (seedDirectory != null) {
             long startTime = System.currentTimeMillis();
 
-            statusLabel.setText("Analysing dir: " + directory);
-            BatchRunner.startRunners(directory);
-            setListOfFiles(directory.listFiles());
+            statusLabel.setText("Analysing dir: " + seedDirectory);
+            BatchRunner.startRunners(seedDirectory);
+            setListOfFiles(seedDirectory.listFiles());
 
             long endTime = System.currentTimeMillis();
             timerLabel.setText("Finished in " + (endTime - startTime) / 1000 + " secs.");
@@ -118,8 +116,8 @@ public class ViewController {
             statusLabel.setText("Analysing file: " + book.getTitle());
 
             if (book.resultsFileExists()) {
-                showPosChart();
-                showDifficultyChart();
+                showPosChart(book);
+                showDifficultyChart(book);
             } else {
                 book.analyseText();
                 statusLabel.setText("Displaying results for: " + book.getTitle());
@@ -134,12 +132,12 @@ public class ViewController {
 
                 if (posChartToggle.isSelected()) {
                     ow.makePosGraph();
-                    showPosChart();
+                    showPosChart(book);
                 }
 
                 if (diffChartToggle.isSelected()) {
                     ow.makeDifficultyGraph();
-                    showDifficultyChart();
+                    showDifficultyChart(book);
                 }
 
                 while (Runner.running) {
@@ -157,30 +155,15 @@ public class ViewController {
     }
 
     /**
-     * Creates a list of files in a directory
-     * @param fileArray Array of files in a directory
+     * Creates a list of files in a seedDirectory
+     * @param fileArray Array of files in a seedDirectory
      */
     private void setListOfFiles(File[] fileArray) {
         for (File file : fileArray) {
             if (file.isDirectory()) {
-                setListOfFiles(file.listFiles(), file.getName());
+                setListOfFiles(file.listFiles());
             } else {
-                addFileToList(file, file.getName());
-            }
-        }
-    }
-
-    /**
-     * Creates a list of files in a directory
-     *
-     * @param fileArray Array of files in a directory
-     */
-    private void setListOfFiles(File[] fileArray, String sub) {
-        for (File file : fileArray) {
-            if (file.isDirectory()) {
-                setListOfFiles(file.listFiles(), file.getName());
-            } else {
-                addFileToList(file, sub);
+                addFileToList(file);
             }
         }
     }
@@ -189,15 +172,16 @@ public class ViewController {
      * Add a single file to sidebar list of files
      * @param file file to add
      */
-    private void addFileToList(File file, String subdir) {
+    private void addFileToList(File file) {
         ObservableList<String> files = resultsFileListView.getItems();
         //TODO add logic to avoid adding items twice if single files were opened beforehand
-        book.setTitleFromText(file);
-        book.subdirectory = subdir;
-        bookMap.put(book.getTitle(), book);
+        Book newBook = new Book();
+        newBook.setTitleFromText(file);
+        newBook.setPath(file);
 
-        if (!files.contains(book.getName())) {
-            files.add(book.getName());
+        if (!files.contains(newBook.getName())) {
+            files.add(newBook.getName());
+            bookMap.put(newBook.getName(), newBook);
             resultsFileListView.setItems(files);
         }
     }
@@ -210,20 +194,16 @@ public class ViewController {
      * Changes the current book by updating images
      */
     private void switchActiveBook(String listItemText) {
-        System.out.println("Switching book to " + listItemText);
-
+        //TODO test more use cases     (File not in subdir)
         Task<Void> task = new Task<Void>() {
             @Override
             public Void call() {
-                System.out.println("Running book switch task");
-                Book cur = new Book();
-                cur.setTitle(listItemText.split(" by ")[0]);
-                cur.setAuthor(listItemText.split(" by ")[1]);
-                book = bookMap.get(cur.getTitle());
-                System.out.println(bookMap.get(cur.getTitle()).getAuthor());
-                System.out.println("ACTIVE: " + book.getName() + " | " + book.subdirectory);
-                showPosChart();
-                showDifficultyChart();
+                // Get new book from map using the book name
+                Book cur = bookMap.get(listItemText);
+                //Set any subdirectories
+                cur.subdirectory = cur.getPath().getParentFile().getName();
+                showPosChart(cur);
+                showDifficultyChart(cur);
                 return null;
             }
         };
@@ -232,22 +212,15 @@ public class ViewController {
 
 
     //TODO get rid of absolute path for images
-    private void showPosChart() {
-        File img = new File("results/img/" + book.subdirectory + "/" + book.getName()
+    private void showPosChart(Book cur) {
+        Image posGraph = new Image("file:results/img/" + cur.subdirectory + "/" + cur.getName()
                 + " POS Distribution Results.jpeg");
-        Image posGraph = new Image("file:results/img/" + book.subdirectory + "/" + book.getName()
-                + " POS Distribution Results.jpeg");
-
-
-        System.out.println("\n[POS CHART UPDATE]\n" + book.getName() + "\nImage path: " +
-                img.getPath() + "\nExists: " + img.exists());
-
 
         posChartImageView.setImage(posGraph);
     }
 
-    private void showDifficultyChart() {
-        Image difficultyChart = new Image("file:results/img/" + book.subdirectory + "/" + book.getName()
+    private void showDifficultyChart(Book cur) {
+        Image difficultyChart = new Image("file:results/img/" + cur.subdirectory + "/" + cur.getName()
                 + " Difficulty Results.jpeg");
 
         difficultyImageView.setImage(difficultyChart);
